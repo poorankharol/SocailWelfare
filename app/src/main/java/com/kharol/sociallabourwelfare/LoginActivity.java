194590ebf334afd2535d1153379353d4e3181279
Pooran Kharol <kharolpooran@gmail.com>
2018-10-07 14:00:32 +0530

    added sPlash Screen

--- app/src/main/java/com/kharol/sociallabourwelfare/LoginActivity.java
+++ app/src/main/java/com/kharol/sociallabourwelfare/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.kharol.sociallabourwelfare;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        },1000);
     }
 }

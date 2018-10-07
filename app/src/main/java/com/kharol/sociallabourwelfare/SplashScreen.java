8bac25391080d12fb281223e16cda3631d805e26
Pooran Kharol <kharolpooran@gmail.com>
2018-10-07 13:47:53 +0530

    added sPlash Screen

--- app/src/main/java/com/kharol/sociallabourwelfare/SplashScreen.java
+++ app/src/main/java/com/kharol/sociallabourwelfare/SplashScreen.java
@@ -1,13 +1,25 @@
 package com.kharol.sociallabourwelfare;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        },3000);
     }
 }

dddb5d53a2212464db6de6331de8c402b91a6b0f
Pooran Kharol <kharolpooran@gmail.com>
2018-10-10 22:40:23 +0530

    Register

---
+++ app/src/main/java/com/kharol/sociallabourwelfare/activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.kharol.sociallabourwelfare.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kharol.sociallabourwelfare.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

0ae3a8cd589c2bd65fc3d63eeab62873f6cb0098
amrat014 <amrat014@gmail.com>
2018-10-20 15:56:33 +0530

    Merge branch 'master' of https://github.com/poorankharol/SocailWelfare
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/kharol/sociallabourwelfare/activities/LoginActivity.java
+++ app/src/main/java/com/kharol/sociallabourwelfare/activities/LoginActivity.java
@@ -1,111 +1,85 @@
 package com.kharol.sociallabourwelfare.activities;
 
+import android.app.AlertDialog;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.kharol.sociallabourwelfare.R;
-import com.kharol.sociallabourwelfare.Service.RequestGenerator;
-import com.kharol.sociallabourwelfare.Service.ResponseListener;
-import com.kharol.sociallabourwelfare.helper.AppConstant;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-
-    EditText edt_email,edt_password;
-    Button login,btnRegister;
+public class LoginActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        edt_email=findViewById(R.id.edt_email);
-        edt_password=findViewById(R.id.edt_password);
-        login=findViewById(R.id.btnlogin);
-        login.setOnClickListener(this);
-        btnRegister=findViewById(R.id.btnRegister);
-        btnRegister.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View v)
-    {
-        if (v==login)
-        {
-            Intent i=new Intent(LoginActivity.this,MainActivity.class);
-            //i.putExtra("id",id);
-            startActivity(i);
-            /*final String email=edt_email.getText().toString();
-            final String password=edt_password.getText().toString();
-//            Log.e("url",AppConstant.LOGIN);
-//            //http://localhost:7080/slw/Login.php
-            StringRequest stringRequest=new StringRequest(Request.Method.POST, AppConstant.LOGIN, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response)
-                {
-                    try
-                    {
-                        JSONObject jsonObject=new JSONObject(response);
-                        if (jsonObject.getString("status").equalsIgnoreCase("1"))
-                        {
-                            String id="";
-                            JSONArray jsonArray=jsonObject.getJSONArray("data");
-                            for (int i=0;i<jsonArray.length();i++)
-                            {
-                                JSONObject object=jsonArray.getJSONObject(i);
-                                id=object.getString("id");
+        final EditText etemail = (EditText) findViewById(R.id.edt_email);
+        final EditText etPassword = (EditText) findViewById(R.id.edt_password);
+        final EditText etNumber=(EditText)findViewById(R.id.edt_number);
+        final TextView tvRegisterLink = (TextView) findViewById(R.id.btnRegister);
+        final Button bLogin = (Button) findViewById(R.id.btnlogin);
+
+        tvRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = etemail.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                // Response received from the server
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                String name = jsonResponse.getString("name");
+                                int number = jsonResponse.getInt("number");
+
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("name", name);
+                                intent.putExtra("number", number);
+                                intent.putExtra("email", email);
+                                intent.putExtra("password",password);
+
+                                LoginActivity.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
                             }
-                            Intent i=new Intent(LoginActivity.this,MainActivity.class);
-                            i.putExtra("id",id);
-                            startActivity(i);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
                     }
+                };
 
-                }
-            }, new Response.ErrorListener()
-            {
-                @Override
-                public void onErrorResponse(VolleyError error)
-                {
-                    Log.e("ERror",error.toString());
-                }
-            }){
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError
-                {
-                    Map<String,String> stringMap=new HashMap<>();
-                    stringMap.put("username",email);
-                    stringMap.put("password",password);
-                    return stringMap;
-                }
-            };
-            RequestQueue requestQueue= Volley.newRequestQueue(LoginActivity.this);
-            requestQueue.add(stringRequest);*/
-        }
-        if (v==btnRegister)
-        {
-            startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
-        }
+                LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
     }
 }
--- app/src/main/java/com/kharol/sociallabourwelfare/activities/RegisterActivity.java
+++ app/src/main/java/com/kharol/sociallabourwelfare/activities/RegisterActivity.java
@@ -1,106 +1,70 @@
 package com.kharol.sociallabourwelfare.activities;
 
+import android.app.AlertDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.kharol.sociallabourwelfare.R;
-import com.kharol.sociallabourwelfare.helper.AppConstant;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
+public class RegisterActivity extends AppCompatActivity {
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
-
-    EditText edt_name,edt_email,edt_number,edt_password;
-    Button Reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        edt_name=findViewById(R.id.edt_name);
-        edt_email=findViewById(R.id.edt_email);
-        edt_number=findViewById(R.id.edt_number);
-        edt_password=findViewById(R.id.edt_password);
-        Reg=findViewById(R.id.btnRegister);
-        Reg.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view)
-    {
-        if (view==Reg)
-        {
-            final String name=edt_name.getText().toString();
-            final String email=edt_email.getText().toString();
-            final String number=edt_number.getText().toString();
-            final String password=edt_password.getText().toString();
-            if (name.equals("") || name.isEmpty())
-            {
-                Toast.makeText(this, "Enter name!!", Toast.LENGTH_SHORT).show();
-            }
-            else if (email.equals("") || email.isEmpty())
-            {
-                Toast.makeText(this, "Enter email!!", Toast.LENGTH_SHORT).show();
-            }
 
 
-            else if (password.equals("") || password.isEmpty())
-            {
-                Toast.makeText(this, "Enter password!!", Toast.LENGTH_SHORT).show();
-            }
-            else {
-                StringRequest request=new StringRequest(Request.Method.POST, AppConstant.REGISTER, new Response.Listener<String>() {
+        final EditText etName = (EditText) findViewById(R.id.edt_name);
+        final EditText etNumber = (EditText) findViewById(R.id.edt_number);
+        final EditText etPassword = (EditText) findViewById(R.id.edt_password);
+        final EditText etemail=(EditText)findViewById(R.id.edt_email);
+        final Button bRegister = (Button) findViewById(R.id.btnRegister);
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = etName.getText().toString();
+                final int number = Integer.parseInt(etNumber.getText().toString());
+                final String email = etemail.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
-                    public void onResponse(String response)
-                    {
-                        try
-                        {
-                            JSONObject jsonObject=new JSONObject(response);
-                            if (jsonObject.getString("status").equals("1"))
-                            {
-                                Intent i=new Intent(RegisterActivity.this,LoginActivity.class);
-                                startActivity(i);
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .setPositiveButton("OK",null)
+                                        .create()
+                                        .show();
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }){
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String,String> stringMap=new HashMap<>();
-                        stringMap.put("name",name);
-                        stringMap.put("email",email);
-                        stringMap.put("number", number);
-                        stringMap.put("password",password);
-                        return stringMap;
                     }
                 };
-                RequestQueue requestQueue= Volley.newRequestQueue(RegisterActivity.this);
-                requestQueue.add(request);
-            }
 
-        }
+                RegisterRequest registerRequest = new RegisterRequest(name,number,email,password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
